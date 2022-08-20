9150ee91eea5c7296dad461909ea1be2af525d16
Shlok Hinge <shlokpoona@gmail.com>
2022-08-20 15:36:30 +0530

    general deploy

--- movie-recommender/src/Components/Trending.js
+++ movie-recommender/src/Components/Trending.js
@@ -1,20 +1,18 @@
 import React, { useState , useEffect} from 'react'
 import Subhead from './Subhead';
 import SingleContent from './SingleComponent';
-import axios from 'axios';
 // import '../Styles/Movies.css'
 
 import Header from '../Components/Header';
-// const url=`https://api.themoviedb.org/3/trending/all/week?api_key=52bfcae0dc91f5f46cb967987f36523a` ;
+const url=`https://api.themoviedb.org/3/trending/all/week?api_key=52bfcae0dc91f5f46cb967987f36523a` ;
 
 
 function Trending() {
 
   const [data, setdata] = useState([])
   const fetchData = async ()=>{
-    const { ans } = await axios.get(
-      'https://api.themoviedb.org/3/trending/all/week?api_key=52bfcae0dc91f5f46cb967987f36523a'
-    );
+      const res=await fetch(url);
+      const ans=await res.json();
       setdata(ans.results);
  
     
