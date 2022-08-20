d974cfb40db8ec6946c591cde9d32f454b9996bc
Shlok Hinge <shlokpoona@gmail.com>
2022-08-20 14:22:53 +0530

    added files

---
+++ movie-recommender/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Header.css'
+function Header() {
+  return (
+    <>
+    <div style={{padding:"0"}} className="headerContainer">
+     <div className='header neonText'>movie time</div>
+     </div>
+  </>
+  )
+}
+
+export default Header
