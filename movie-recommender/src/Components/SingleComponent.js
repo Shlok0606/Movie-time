b23fa4ab30bef4da9315571612a71e4fbf07d6ba
Shlok Hinge <shlokpoona@gmail.com>
2022-08-20 17:37:22 +0530

    Focused  on responsiveness

--- movie-recommender/src/Components/SingleComponent.js
+++ movie-recommender/src/Components/SingleComponent.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../Styles/SingleComponent.css"
 import { Link } from 'react-router-dom'
 import { GlobalContext } from './contex'
+import { useState, useEffect } from 'react'
 const SingleComponent = ({
   id,
   poster,
@@ -12,7 +13,49 @@ const SingleComponent = ({
   page_type
 }) => {
   const {updateType} =GlobalContext();
+
+const [wordDate, setwordDate] = useState("");
+
+var m=["Jan" ,"Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , 
+"Sep" , "Oct" , "Nov" , "Dec"]
+
+
+useEffect(() => {
  
+  var year="";
+  var day="";
+   var month="";
+   
+ var i=0;
+ while(i<date.length&&date[i]!=='-'){
+   year+=date[i];
+   i++;
+ }
+i++;
+
+while(i<date.length&&date[i]!=='-'){
+  month+=date[i];
+  i++;
+}
+i++;
+var monthInt=parseInt(month,10);
+month=m[monthInt-1];
+
+while(i<date.length){
+  day+=date[i];
+  i++;
+}
+console.log(date, day , monthInt , year);
+
+setwordDate(`${day} ${month}, ${year}`);
+  if(!date){
+    setwordDate("");
+  }
+
+ 
+}, [])
+
+
   return(
    
     <Link  onClick={()=>{updateType(media_type)}} style={ {textDecoration: 'none'}} to={`/${id}`}   className='cardContainer'>
@@ -29,7 +72,7 @@ const SingleComponent = ({
          { page_type==="trending"?media_type:(vote_average?vote_average:"-")+"/10"}
         </div>
         <div className="cardDate">
-          {date}
+          {wordDate}
         </div>
       </div>
     </Link>
